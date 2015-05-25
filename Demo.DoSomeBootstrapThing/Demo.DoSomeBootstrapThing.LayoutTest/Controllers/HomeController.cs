684791d91463f430024b7aabe9cfd98f644ff58a
dance2die <dontbothermewithspam@gmail.com>
2015-05-25 09:54:43 -0400

    Crated a barebone page of "Mint" create a new account page

--- Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.LayoutTest/Controllers/HomeController.cs
+++ Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.LayoutTest/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using Demo.DoSomeBootstrapThing.LayoutTest.Models;
 
 namespace Demo.DoSomeBootstrapThing.LayoutTest.Controllers
 {
@@ -11,6 +9,25 @@ namespace Demo.DoSomeBootstrapThing.LayoutTest.Controllers
 		public ActionResult Index()
 		{
 			return View();
+		}
+
+		public ActionResult Mint()
+		{
+			MintModel model = new MintModel
+			{
+				CountryListItems = new SelectList(GetCountries(), "Id", "Name")
+			};
+			return View(model);
+		}
+
+		private IEnumerable<Country> GetCountries()
+		{
+			return new List<Country>
+			{
+				new Country {Id = 1, Name = "USA"},
+				new Country {Id = 2, Name = "Korea"},
+				new Country {Id = 3, Name = "Uganda"},
+			};
 		}
 	}
 }
---
+++ Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.LayoutTest/Models/MintModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Demo.DoSomeBootstrapThing.LayoutTest.Models
+{
+	public class MintModel
+	{
+		public IEnumerable<SelectListItem> CountryListItems { get; set; }
+	}
+}
