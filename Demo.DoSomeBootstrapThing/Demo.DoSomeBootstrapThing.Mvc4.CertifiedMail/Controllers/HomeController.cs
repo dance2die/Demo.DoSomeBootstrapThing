e22ebcd92d09182274ba2d7e336faf43c261bc49
dance2die <dontbothermewithspam@gmail.com>
2015-07-11 22:28:52 -0400

    can't get "OR" vertical aligned right...

--- Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail/Controllers/HomeController.cs
+++ Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail/Controllers/HomeController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail.Models;
 
 namespace Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View(new HomeModel());
+		}
 
-    }
+		public ActionResult SearchByClient(HomeModel model)
+		{
+			return View("Index", model);
+		}
+
+		public ActionResult Upload(HttpPostedFileBase file)
+		{
+			return View("Index", new HomeModel());
+		}
+	}
 }
---
+++ Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+
+namespace Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail.Models
+{
+	public class HomeModel
+	{
+		public string FileNumber { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		//public HttpPostedFileBase File { get; set; }
+	}
+}
