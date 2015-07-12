beaf90da5bdf85c5394bb79293cd0c70b27d5cc5
dance2die <dontbothermewithspam@gmail.com>
2015-07-11 20:14:02 -0400

    added "Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail"

---
+++ Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
