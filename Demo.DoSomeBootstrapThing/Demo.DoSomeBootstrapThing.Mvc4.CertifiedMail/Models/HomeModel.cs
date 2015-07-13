42e1bfbb8e90f774c27707590fd2914611fc5f29
dance2die <dontbothermewithspam@gmail.com>
2015-07-13 13:32:48 -0400

    Removed form-group css and updated grid layout lengths

--- Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail/Models/HomeModel.cs
+++ Demo.DoSomeBootstrapThing/Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail/Models/HomeModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail.Models
+﻿using System.ComponentModel;
+
+namespace Demo.DoSomeBootstrapThing.Mvc4.CertifiedMail.Models
 {
 	public class HomeModel
 	{
+		[DisplayName("File #")]
 		public string FileNumber { get; set; }
+		[DisplayName("First Name")]
 		public string FirstName { get; set; }
+		[DisplayName("Last Name")]
 		public string LastName { get; set; }
 
 		//public HttpPostedFileBase File { get; set; }
